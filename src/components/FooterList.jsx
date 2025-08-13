a1feb24d08a510ba76cee492c6e4ca5613588b5f
mdsalathossain0 <mdsalathossain0@gmail.com>
2025-08-13 18:35:28 +0600

    coursor add

--- src/components/FooterList.jsx
+++ src/components/FooterList.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const FooterList = ({text,className}) => {
   return (
-    <li className={`list-none text-[#ffffffcc] text-sm font-pop font-normal leading-4 ${className}`}>{text}</li>
+    <li className={`list-none text-[#ffffffcc] text-sm font-pop font-normal leading-4 cursor-pointer ${className}`}>{text}</li>
   )
 }
 
